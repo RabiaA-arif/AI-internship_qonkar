46d26720e127162a49b47f8e8d6adc67f6447bc6
RabiaA-arif <rabiaarif694@gmail.com>
2025-05-26 18:03:51 +0500

    changes

--- cold_email_generator/email/app/main.py
+++ cold_email_generator/email/app/main.py
@@ -1,13 +1,8 @@
 import streamlit as st
-import langchain_community
-
 from langchain_community.document_loaders import WebBaseLoader
 from chain import Chain
 from portfolio import Portfolio
 from utils import clean_text
-
-
-
 def create_streamlit_app(llm, portfolio, clean_text):
     st.title("📧 Cold Mail Generator")
     url_input = st.text_input("Enter a URL:", value="https://www.guru.com/m/find/freelance-jobs/web-data-scraping/")
